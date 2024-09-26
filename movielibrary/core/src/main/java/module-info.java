d1e77a9b6ade5abbe2bcdfffc6e52131717178a7
Jennica Duong <jennicad@stud.ntnu.no>
2024-09-26 14:26:31 +0200

    chore: :sparkles: Include jackson.core and jackson-databind packages in core/module-info.java
    Added jackson packages: com.fasterxml.jackson.core and com.fasterxml.jackson.databind to module-info.java in core folder to enable JSON processing (serialization and deserialization) in a Java module system.
    
    Co-authored-by: Helle Hornes Fystro

--- movielibrary/core/src/main/java/module-info.java
+++ movielibrary/core/src/main/java/module-info.java
@@ -1,5 +1,10 @@
 module movielibrary.core {
 
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
     exports movielibrary.core;
     exports movielibrary.json.internal;
+
+    opens movielibrary.core to com.fasterxml.jackson.databind;
 }
