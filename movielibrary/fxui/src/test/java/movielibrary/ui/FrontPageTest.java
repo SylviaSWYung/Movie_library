416869828584b7166218fa889fb362ab175213dd
Jennica Duong <jennicad@stud.ntnu.no>
2024-10-08 17:34:17 +0200

    test: :white_check_mark: Add and uncomment more tests in FrontPageTest.java and fix updated fxid references
    Added testLoadPage method to indirectly test the catch clause in loadPage method from FrontPageController.java. Uncomment the moreInfoBtn_NoMovieSelected method. Fixed fxid references to the updated ids.
    
    Co-authored-by: Helle Hornes Fystro

--- movielibrary/fxui/src/test/java/movielibrary/ui/FrontPageTest.java
+++ movielibrary/fxui/src/test/java/movielibrary/ui/FrontPageTest.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,9 @@ import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.util.WaitForAsyncUtils;
 
 import static org.testfx.assertions.api.Assertions.assertThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.testfx.api.FxAssert.verifyThat;
 import java.io.IOException;
 
@@ -23,8 +27,8 @@ import java.io.IOException;
 public class FrontPageTest extends ApplicationTest {
 
     private Parent root;
+    private FrontPageController controller;
 
-    
 
     // Loads the FrontPage.fxml file
     @Override
@@ -36,57 +40,71 @@ public class FrontPageTest extends ApplicationTest {
         stage.show();
     }
 
+    // initializes the FrontPageController
+    @BeforeEach
+    public void setUp() {
+        controller = new FrontPageController();
+    }
+
     // Tests to see if the choosen movie is handled correctly
     @Test
+    @DisplayName("Choosing a movie from the scrollbar test")
     public void testChooseMovie() {
-        clickOn("#MovieScrollBar");
+        clickOn("#movieScrollBar");
         WaitForAsyncUtils.waitForFxEvents();
 
         clickOn("Loverboy");
         WaitForAsyncUtils.waitForFxEvents();
 
-        verifyThat("#MovieScrollBar", (ChoiceBox<String> choiceBox) -> 
+        verifyThat("#movieScrollBar", (ChoiceBox<String> choiceBox) -> 
         "Loverboy".equals(choiceBox.getValue()));
 
     }
 
 
     // Tests the "More Info" button if no movie is selected
-    // @Test
-    // @DisplayName("More info button test - no movie selected")
-    // public void moreInfoBtn_NoMovieSelected() throws IOException {
-    //     clickOn("#MoreInfobtn");
-    //     WaitForAsyncUtils.waitForFxEvents();
+    @Test
+    @DisplayName("More info button test - no movie selected")
+    public void moreInfoBtn_NoMovieSelected() throws IOException {
+        clickOn("#moreInfobtn");
+        WaitForAsyncUtils.waitForFxEvents();
 
-    //     verifyThat(".alert", NodeMatchers.isVisible());
-    //     verifyThat(".alert .content", hasText("Please choose a movie from the scrollbar menu"));
-    // }
+        verifyThat(".alert", NodeMatchers.isVisible());
+        verifyThat(".alert .content", hasText("Please choose a movie from the scrollbar menu"));
+    }
 
     // Tests to see if the Movie Title, Movie Description and Movie Durations is handled correctly.
     @Test
     @DisplayName("More info button test")
-    public void testMoreInfobtn() throws IOException {
-        clickOn("#MovieScrollBar");
+      public void testMoreInfobtn() throws IOException {
+        clickOn("#movieScrollBar");
         WaitForAsyncUtils.waitForFxEvents();
 
         clickOn("Loverboy");
         WaitForAsyncUtils.waitForFxEvents();
 
-        clickOn("#MoreInfobtn");
+        clickOn("#moreInfobtn");
         WaitForAsyncUtils.waitForFxEvents();
 
-        verifyThat("#MovieTitle", NodeMatchers.isVisible());
-        TextField movieTitleField = lookup("#MovieTitle").queryAs(TextField.class);
+        verifyThat("#movieTitleInPage", NodeMatchers.isVisible());
+        TextField movieTitleField = lookup("#movieTitleInPage").queryAs(TextField.class);
         assertThat(movieTitleField).hasText("Loverboy");
 
-        verifyThat("#Summary", NodeMatchers.isVisible());
-        TextArea movieSummaryField = lookup("#Summary").queryAs(TextArea.class);
+        verifyThat("#summary", NodeMatchers.isVisible());
+        TextArea movieSummaryField = lookup("#summary").queryAs(TextArea.class);
         assertThat(movieSummaryField).hasText("Based on a true story, about a boy who marries his crush");
 
-        verifyThat("#MovieDuration", NodeMatchers.isVisible());
-        TextField movieDurationField = lookup("#MovieDuration").queryAs(TextField.class);
+        verifyThat("#movieDuration", NodeMatchers.isVisible());
+        TextField movieDurationField = lookup("#movieDuration").queryAs(TextField.class);
         assertThat(movieDurationField).hasText("30.0");
     }
 
-
+    // Indirectly test the error when running the loadPage method with a invalid FXML file
+    @Test
+    @DisplayName("loadPage method error handling test")
+      public void testLoadPage() {
+        assertThrows(IllegalStateException.class, () -> {
+            controller.loadPage("Fake.fxml", "Fake movie title", "Fake movie description", 100.0);
+        });
+  }
 }
