37eb3be22b956307fe8e3d694e8cbe0f4b3b459b
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2024-10-10 13:08:54 +0200

    style: :art: Correct indentation in AppTest.java and FrontPageTest.java
    Correct indentation from 4 to 2 spaces for each indent in AppTest.java and FrontPageTest.java
    
    Co-authored-by: Sylvia Yung

--- movielibrary/fxui/src/test/java/movielibrary/ui/FrontPageTest.java
+++ movielibrary/fxui/src/test/java/movielibrary/ui/FrontPageTest.java
@@ -26,85 +26,84 @@ import java.io.IOException;
 
 public class FrontPageTest extends ApplicationTest {
 
-    private Parent root;
-    private FrontPageController controller;
+  private Parent root;
+  private FrontPageController controller;
 
 
-    // Loads the FrontPage.fxml file
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("FrontPage.fxml"));
-        root = fxmlLoader.load();    
+  // Loads the FrontPage.fxml file
+  @Override
+  public void start(Stage stage) throws Exception {
+    FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("FrontPage.fxml"));
+    root = fxmlLoader.load();    
 
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
+    stage.setScene(new Scene(root));
+    stage.show();
+  }
 
-    // initializes the FrontPageController
-    @BeforeEach
-    public void setUp() {
-        controller = new FrontPageController();
-    }
+  // initializes the FrontPageController
+  @BeforeEach
+  public void setUp() {
+    controller = new FrontPageController();
+  }
 
-    // Tests to see if the choosen movie is handled correctly
-    @Test
-    @DisplayName("Choosing a movie from the scrollbar test")
-    public void testChooseMovie() {
-        clickOn("#movieScrollBar");
-        WaitForAsyncUtils.waitForFxEvents();
+  // Tests to see if the choosen movie is handled correctly
+  @Test
+  @DisplayName("Choosing a movie from the scrollbar test")
+  public void testChooseMovie() {
+    clickOn("#movieScrollBar");
+    WaitForAsyncUtils.waitForFxEvents();
 
-        clickOn("Loverboy");
-        WaitForAsyncUtils.waitForFxEvents();
+    clickOn("Loverboy");
+    WaitForAsyncUtils.waitForFxEvents();
 
-        verifyThat("#movieScrollBar", (ChoiceBox<String> choiceBox) -> 
-        "Loverboy".equals(choiceBox.getValue()));
+    verifyThat("#movieScrollBar", (ChoiceBox<String> choiceBox) -> 
+    "Loverboy".equals(choiceBox.getValue()));
+  }
 
-    }
 
+  // Tests the "More Info" button if no movie is selected
+  @Test
+  @DisplayName("More info button test - no movie selected")
+  public void moreInfoBtn_NoMovieSelected() throws IOException {
+    clickOn("#moreInfobtn");
+    WaitForAsyncUtils.waitForFxEvents();
 
-    // Tests the "More Info" button if no movie is selected
-    @Test
-    @DisplayName("More info button test - no movie selected")
-    public void moreInfoBtn_NoMovieSelected() throws IOException {
-        clickOn("#moreInfobtn");
-        WaitForAsyncUtils.waitForFxEvents();
+    verifyThat(".alert", NodeMatchers.isVisible());
+    verifyThat(".alert .content", hasText("Please choose a movie from the scrollbar menu"));
+  }
 
-        verifyThat(".alert", NodeMatchers.isVisible());
-        verifyThat(".alert .content", hasText("Please choose a movie from the scrollbar menu"));
-    }
+  // Tests to see if the Movie Title, Movie Description and Movie Durations is handled correctly.
+  @Test
+  @DisplayName("More info button test")
+  public void testMoreInfobtn() throws IOException {
+    clickOn("#movieScrollBar");
+    WaitForAsyncUtils.waitForFxEvents();
 
-    // Tests to see if the Movie Title, Movie Description and Movie Durations is handled correctly.
-    @Test
-    @DisplayName("More info button test")
-      public void testMoreInfobtn() throws IOException {
-        clickOn("#movieScrollBar");
-        WaitForAsyncUtils.waitForFxEvents();
+    clickOn("Loverboy");
+    WaitForAsyncUtils.waitForFxEvents();
 
-        clickOn("Loverboy");
-        WaitForAsyncUtils.waitForFxEvents();
+    clickOn("#moreInfobtn");
+    WaitForAsyncUtils.waitForFxEvents();
 
-        clickOn("#moreInfobtn");
-        WaitForAsyncUtils.waitForFxEvents();
+    verifyThat("#movieTitleInPage", NodeMatchers.isVisible());
+    TextField movieTitleField = lookup("#movieTitleInPage").queryAs(TextField.class);
+    assertThat(movieTitleField).hasText("Loverboy");
 
-        verifyThat("#movieTitleInPage", NodeMatchers.isVisible());
-        TextField movieTitleField = lookup("#movieTitleInPage").queryAs(TextField.class);
-        assertThat(movieTitleField).hasText("Loverboy");
+    verifyThat("#summary", NodeMatchers.isVisible());
+    TextArea movieSummaryField = lookup("#summary").queryAs(TextArea.class);
+    assertThat(movieSummaryField).hasText("Based on a true story, about a boy who marries his crush");
 
-        verifyThat("#summary", NodeMatchers.isVisible());
-        TextArea movieSummaryField = lookup("#summary").queryAs(TextArea.class);
-        assertThat(movieSummaryField).hasText("Based on a true story, about a boy who marries his crush");
+    verifyThat("#movieDuration", NodeMatchers.isVisible());
+    TextField movieDurationField = lookup("#movieDuration").queryAs(TextField.class);
+    assertThat(movieDurationField).hasText("30.0");
+  }
 
-        verifyThat("#movieDuration", NodeMatchers.isVisible());
-        TextField movieDurationField = lookup("#movieDuration").queryAs(TextField.class);
-        assertThat(movieDurationField).hasText("30.0");
-    }
-
-    // Indirectly test the error when running the loadPage method with a invalid FXML file
-    @Test
-    @DisplayName("loadPage method error handling test")
-      public void testLoadPage() {
-        assertThrows(IllegalStateException.class, () -> {
-            controller.loadPage("Fake.fxml", "Fake movie title", "Fake movie description", 100.0);
-        });
+  // Indirectly test the error when running the loadPage method with a invalid FXML file
+  @Test
+  @DisplayName("loadPage method error handling test")
+  public void testLoadPage() {
+    assertThrows(IllegalStateException.class, () -> {
+      controller.loadPage("Fake.fxml", "Fake movie title", "Fake movie description", 100.0);
+    });
   }
 }
