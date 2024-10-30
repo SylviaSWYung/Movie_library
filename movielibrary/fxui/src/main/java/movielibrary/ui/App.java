013092f45a3970110c6a13aa76ed8ad0fd465971
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2024-10-30 15:05:53 +0100

    feat: :sparkles: Implement supportHeadless method in App.java
    Implement supportHeadless method in App.java to run tests headless
    
    Co-authored-by: Helle Hornes Fystro

--- movielibrary/fxui/src/main/java/movielibrary/ui/App.java
+++ movielibrary/fxui/src/main/java/movielibrary/ui/App.java
@@ -13,6 +13,19 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     
+  /**
+   * Helper method used by tests needing to run headless.
+   */
+  public static void supportHeadless() {
+    if (Boolean.getBoolean("headless")) {
+      System.setProperty("testfx.robot", "glass");
+      System.setProperty("testfx.headless", "true");
+      System.setProperty("prism.order", "sw");
+      System.setProperty("prism.text", "t2k");
+      System.setProperty("java.awt.headless", "true");
+    }
+  }
+
   @Override
   public void start(Stage primaryStage) throws IOException {
     try {
