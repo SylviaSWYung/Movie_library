fa924cf22d7d4794f567a2007c092fe7c3ddef00
Jennica Duong <jennica.duong@ntnu.no>
2024-11-12 14:14:53 +0100

    test: :white_check_mark: Create and implement MovieLibraryApplicationTest.java to test MovieLibraryApplication.java

--- movielibrary/springboot/restserver/src/main/java/movielibrary/springboot/restserver/MovieNotFoundAdvice.java
+++ movielibrary/springboot/restserver/src/main/java/movielibrary/springboot/restserver/MovieNotFoundAdvice.java
@@ -1,5 +1,7 @@
 package movielibrary.springboot.restserver;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,20 +9,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * The {@code MovieNotFoundAdvice} class handles
- * the {@link MovieNotFoundException} thrown by the application.
- * HTTP response with a NOT_FOUND status. Inidicates that the movie could not be found.
- *
- * @param ex The {@code MovieNotFoundException} that was thrown
- * @return A {@code String} containing the error message from the exception
+ * The {@code MovieNotFoundAdvice} class handles {@link MovieNotFoundException}.
  */
 @ControllerAdvice
 public class MovieNotFoundAdvice {
 
+  /**
+   * The {@code MovieNotFoundAdvice} class handles
+   * the {@link MovieNotFoundException} thrown by the application.
+   * HTTP response with a NOT_FOUND status. Inidicates that the movie could not be found.
+   *
+   * @param ex The {@code MovieNotFoundException} that was thrown
+   * @return A {@code String} containing the error message from the exception
+   */
   @ResponseBody
   @ExceptionHandler(MovieNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  String movieNotFoundHandler(MovieNotFoundException ex) {
-    return ex.getMessage();
+  public Map<String, String> handleMovieNotFoundException(MovieNotFoundException ex) {
+    Map<String, String> response = new HashMap<>();
+    response.put("message", ex.getMessage());
+    return response;
   }
 }
